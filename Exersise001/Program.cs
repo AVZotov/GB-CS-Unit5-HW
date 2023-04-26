4e120d69a2efee1fb343f7babe4dad8891b9f129
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-26 19:46:17 +0300

    Exersise001 project added

---
+++ Exersise001/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] GetArray()
+{
+    int minArrayLength = 5;
+    int maxArrayLength = 9;
+    int[] array = new int[new Random().Next(minArrayLength, maxArrayLength + 1)];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+
+    return array;
+}
+
+int GetEvenSum(int[]array)
+{
+int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sum += array[i];
+        }
+    }
+
+    return sum;
+}
+
+int[] array = GetArray();
+
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine($"Summ of even in array = {GetEvenSum(array)}");

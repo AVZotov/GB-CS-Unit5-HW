8ea352cf5c3f31109ff4c12e7336fc7048f98df7
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-27 16:05:07 +0300

    Exersise003 project added

---
+++ Exersise003/Program.cs
@@ -0,0 +1,54 @@
+﻿double[] GetArray()
+{
+    int minArrayLength = 5;
+    int maxArrayLength = 9;
+    int minValue = -10;
+    int maxValue = 10;
+
+    double[] array = new double[new Random().Next(minArrayLength, maxArrayLength + 1)];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        int divider = new Random().Next(1, 10);
+        double value = (double) new Random().Next(minValue, maxValue) / (double) divider;
+        array[i] = Math.Round(value, 1);
+    }
+
+    return array;
+}
+
+double GetMinValue(double[] array)
+{
+    double minValue = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < minValue)
+        {
+            minValue = array[i];
+        }
+    }
+
+    return minValue;
+}
+
+double GetMaxValue(double[] array)
+{
+    double maxValue = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > maxValue)
+        {
+            maxValue = array[i];
+        }
+    }
+
+    return maxValue;
+}
+
+double[] array = GetArray();
+
+Console.WriteLine(string.Join(", ", array));
+double difference = GetMaxValue(array) - GetMinValue(array);
+Console.WriteLine($"Difference between max value and min value in array = {difference}");
